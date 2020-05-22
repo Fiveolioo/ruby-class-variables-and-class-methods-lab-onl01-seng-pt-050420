4ab566b5ac81fe9d3f208a61f83206c9844154f4
Richard Cho <96richardohc@gmail.com>
2020-05-22 08:33:35 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,6 +2,7 @@ class Song
 attr_accessor :name, :artist, :genre
 @@count = 0
 @@artists = []
+@@genres = []
 
   def initialize(name, artist, genre)
   @name = name 
@@ -9,5 +10,6 @@ attr_accessor :name, :artist, :genre
   @genre = genre
   @@count += 1
   @@artists << artist
+  @@genres << genre
   end
 end
