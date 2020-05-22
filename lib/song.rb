2cf1773c879f17a5c3198ef9a882c153a1d1b08c
Richard Cho <96richardohc@gmail.com>
2020-05-22 09:03:29 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -27,13 +27,17 @@ attr_accessor :name, :artist, :genre
   
   def self.genre_count
     genre_count = {}
-    @@genres.each do |genre|
-      if genre_count[genre]
-        genre_count[genre] += 1 
+    @@genres.each do |a|
+      if genre_count[a]
+        genre_count[a] += 1 
       else
         genre_count[genre] = 1
       end
     end
     genre_count
   end
+  
+  def self.artist_count
+    artist_count = {}
+    @@artists.each do 
 end
